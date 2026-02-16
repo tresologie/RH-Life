ea7fec1af74a396dfc6989f8afbd307b65f4b755
tresologie <tresorira2022@gmail.com>
2026-02-16 18:36:33 +0200

    Suppression des fichiers unitiles

--- ClassTeacher/index.php
+++ ClassTeacher/index.php
@@ -45,7 +45,8 @@ include '../Includes/session.php';
         <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Statistiques du <?php echo $todaysDate = date("d-m-Y");?>(<?php echo $rrw['className'];?>)</h1>
+            <h1 class="h3 mb-0 text-gray-800">Statistiques du <?php echo $todaysDate = date("d-m-Y");?>
+            (<b><?php echo $rrw['className'];?></b>)</h1>
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="./">Accueil</a></li>
               <li class="breadcrumb-item active" aria-current="page">Tableau de bord</li>
